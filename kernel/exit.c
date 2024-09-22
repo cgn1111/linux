aaa1c76ec805b4262399e92f8fe97312482b5892
cgn1111 <129592547+cgn1111@users.noreply.github.com>
2024-09-22 16:05:13 +0300

    Update exit.c
    update(?)

--- kernel/exit.c
+++ kernel/exit.c
@@ -1915,23 +1915,25 @@ COMPAT_SYSCALL_DEFINE4(wait4,
 }
 
 COMPAT_SYSCALL_DEFINE5(waitid,
-		int, which, compat_pid_t, pid,
-		struct compat_siginfo __user *, infop, int, options,
-		struct compat_rusage __user *, uru)
+			int, which, compat_pid_t, pid,
+			struct compat_siginfo __user *, infop, int, options,
+			struct compat_rusage __user *, uru)
 {
 	struct rusage ru;
 	struct waitid_info info = {.status = 0};
 	long err = kernel_waitid(which, pid, &info, options, uru ? &ru : NULL);
 	int signo = 0;
+
 	if (err > 0) {
 		signo = SIGCHLD;
 		err = 0;
+
 		if (uru) {
-			/* kernel_waitid() overwrites everything in ru */
 			if (COMPAT_USE_64BIT_TIME)
 				err = copy_to_user(uru, &ru, sizeof(ru));
 			else
 				err = put_compat_rusage(&ru, uru);
+
 			if (err)
 				return -EFAULT;
 		}
@@ -1943,18 +1945,24 @@ COMPAT_SYSCALL_DEFINE5(waitid,
 	if (!user_write_access_begin(infop, sizeof(*infop)))
 		return -EFAULT;
 
-	unsafe_put_user(signo, &infop->si_signo, Efault);
-	unsafe_put_user(0, &infop->si_errno, Efault);
-	unsafe_put_user(info.cause, &infop->si_code, Efault);
-	unsafe_put_user(info.pid, &infop->si_pid, Efault);
-	unsafe_put_user(info.uid, &infop->si_uid, Efault);
-	unsafe_put_user(info.status, &infop->si_status, Efault);
+	if (unsafe_put_user(signo, &infop->si_signo, Efault) ||
+	    unsafe_put_user(0, &infop->si_errno, Efault) ||
+	    unsafe_put_user(info.cause, &infop->si_code, Efault) ||
+	    unsafe_put_user(info.pid, &infop->si_pid, Efault) ||
+	    unsafe_put_user(info.uid, &infop->si_uid, Efault) ||
+	    unsafe_put_user(info.status, &infop->si_status, Efault)) {
+		user_write_access_end();
+		return -EFAULT;
+	}
+
 	user_write_access_end();
 	return err;
+
 Efault:
 	user_write_access_end();
 	return -EFAULT;
 }
+
 #endif
 
 /*
